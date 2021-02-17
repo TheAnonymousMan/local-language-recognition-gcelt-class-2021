36ae3b23946499ca37790d73b4be5a7090763a8e
Souporno Ghosh <souporno2000@gmail.com>
2021-02-17 17:58:26 +0530

    Update firstScript.py

--- Scripts/firstScript.py
+++ Scripts/firstScript.py
@@ -9,55 +9,72 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.preprocessing import image
 from keras.models import load_model
+from keras.optimizers import Adam
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 # this needs to change
 trdata = ImageDataGenerator()
-traindata = trdata.flow_from_directory(directory="data",target_size=(224,224))
+traindata = trdata.flow_from_directory(
+    directory=".././Datasets/writerPair1/0-10/train", target_size=(224, 224))
 tsdata = ImageDataGenerator()
-testdata = tsdata.flow_from_directory(directory="test", target_size=(224,224))
+testdata = tsdata.flow_from_directory(directory=".././Datasets/writerPair1/0-10/test", target_size=(224, 224))
 
 model = Sequential()
 
-model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(input_shape=(224, 224, 3), filters=64,
+                 kernel_size=(3, 3), padding="same", activation="relu"))
+model.add(Conv2D(filters=64, kernel_size=(3, 3),
+                 padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
-model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=128, kernel_size=(
+    3, 3), padding="same", activation="relu"))
+model.add(Conv2D(filters=128, kernel_size=(
+    3, 3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
-model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=256, kernel_size=(
+    3, 3), padding="same", activation="relu"))
+model.add(Conv2D(filters=256, kernel_size=(
+    3, 3), padding="same", activation="relu"))
+model.add(Conv2D(filters=256, kernel_size=(
+    3, 3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=512, kernel_size=(
+    3, 3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(
+    3, 3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(
+    3, 3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+model.add(Conv2D(filters=512, kernel_size=(
+    3, 3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(
+    3, 3), padding="same", activation="relu"))
+model.add(Conv2D(filters=512, kernel_size=(
+    3, 3), padding="same", activation="relu"))
+model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(units=4096,activation="relu"))
-model.add(Dense(units=4096,activation="relu"))
+model.add(Dense(units=4096, activation="relu"))
+model.add(Dense(units=4096, activation="relu"))
 model.add(Dense(units=2, activation="softmax"))
 
-from keras.optimizers import Adam
 opt = Adam(lr=0.001)
-model.compile(optimizer=opt, loss=keras.losses, metrics=['accuracy'])
+model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
 
 model.summary()
 
-checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
-early = EarlyStopping(monitor='val_acc', min_delta=0, patience=20, verbose=1, mode='auto')
-hist = model.fit_generator(steps_per_epoch=100,generator=traindata, validation_data= testdata, validation_steps=10,epochs=100,callbacks=[checkpoint,early])
+checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1,
+                             save_best_only=True, save_weights_only=False, mode='auto', period=1)
+early = EarlyStopping(monitor='val_acc', min_delta=0,
+                      patience=20, verbose=1, mode='auto')
+hist = model.fit_generator(steps_per_epoch=100, generator=traindata, validation_data=testdata,
+                           validation_steps=10, epochs=100, callbacks=[checkpoint, early])
 
 plt.plot(hist.history["acc"])
 plt.plot(hist.history['val_acc'])
@@ -66,10 +83,10 @@ plt.plot(hist.history['val_loss'])
 plt.title("model accuracy")
 plt.ylabel("Accuracy")
 plt.xlabel("Epoch")
-plt.legend(["Accuracy","Validation Accuracy","loss","Validation Loss"])
+plt.legend(["Accuracy", "Validation Accuracy", "loss", "Validation Loss"])
 plt.show()
 
-img = image.load_img("image.jpeg",target_size=(224,224))
+img = image.load_img("image.jpeg", target_size=(224, 224))
 img = np.asarray(img)
 plt.imshow(img)
 img = np.expand_dims(img, axis=0)
@@ -80,4 +97,3 @@ if output[0][0] > output[0][1]:
     print("cat")
 else:
     print('dog')
-
