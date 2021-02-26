1ceb26814e4e8b56ec8f6c4a9a761f0a9ecc2613
Souporno Ghosh <souporno2000@gmail.com>
2021-02-26 14:17:15 +0530

    Update VGG16_train.py

--- Scripts/VGG16_train.py
+++ Scripts/VGG16_train.py
@@ -7,27 +7,28 @@ from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
+import seaborn as sns
+sns.set()  # souporno: why the ';'?
 
 
-
-model = VGG16(weights='imagenet', include_top=False) #top is not included as we are
-                                                     #dealing with only feature extraction
+# top is not included as we are
+model = VGG16(weights='imagenet', include_top=False)
+# dealing with only feature extraction
 model.summary()
 
 
-
 def image_loader(img_path):
-  img = image.load_img(img_path, target_size=(224, 224))
-  #print(img.shape) gives error as the object is 'Image', not array
-  plt.imshow(img)
-  img_data = image.img_to_array(img)
-  print(img_data.shape)
-  img_data = np.expand_dims(img_data, axis=0)
-  print(img_data.shape)
-  img_data = preprocess_input(img_data)
-  print(img_data.shape)
-  return img_data
+    img = image.load_img(img_path, target_size=(224, 224))
+    # print(img.shape) gives error as the object is 'Image', not array
+    plt.imshow(img)
+    img_data = image.img_to_array(img)
+    print(img_data.shape)
+    img_data = np.expand_dims(img_data, axis=0)
+    print(img_data.shape)
+    img_data = preprocess_input(img_data)
+    print(img_data.shape)
+    return img_data
+
 
 img_path = '../Datasets/writerPair1/0-10/train/0000_01_0.tif'
 img_data = image_loader(img_path)
