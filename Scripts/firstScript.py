dd81861d7ddc0df78dd0f1ca551cb2158136a957
Souporno Ghosh <souporno2000@gmail.com>
2021-03-26 02:19:13 +0530

    Changed a lot of things

--- Scripts/firstScript.py
+++ Scripts/firstScript.py
@@ -14,19 +14,19 @@ from keras.optimizers import Adam
 import numpy as np
 import matplotlib.pyplot as plt
 
-# this needs to change
 trdata = ImageDataGenerator()
 traindata = trdata.flow_from_directory(
-    directory=".././Datasets/writerPair1/0-10/train", target_size=(224, 224))
+    directory="./train", target_size=(224, 224))
 tsdata = ImageDataGenerator()
-testdata = tsdata.flow_from_directory(directory=".././Datasets/writerPair1/0-10/test", target_size=(224, 224))
+testdata = tsdata.flow_from_directory(
+    directory="./test", target_size=(224, 224))
 
 model = Sequential()
 
 model.add(Conv2D(input_shape=(224, 224, 3), filters=64,
-                 kernel_size=(3, 3), padding="same", activation="relu"))
+    kernel_size=(3, 3), padding="same", activation="relu"))
 model.add(Conv2D(filters=64, kernel_size=(3, 3),
-                 padding="same", activation="relu"))
+    padding="same", activation="relu"))
 model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
 
 model.add(Conv2D(filters=128, kernel_size=(
@@ -65,16 +65,17 @@ model.add(Dense(units=4096, activation="relu"))
 model.add(Dense(units=2, activation="softmax"))
 
 opt = Adam(lr=0.001)
-model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
+model.compile(optimizer=opt,
+        loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
 
 model.summary()
 
 checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1,
-                             save_best_only=True, save_weights_only=False, mode='auto', period=1)
+                        save_best_only=True, save_weights_only=False, mode='auto', period=1)
 early = EarlyStopping(monitor='val_acc', min_delta=0,
-                      patience=20, verbose=1, mode='auto')
+                        patience=20, verbose=1, mode='auto')
 hist = model.fit_generator(steps_per_epoch=100, generator=traindata, validation_data=testdata,
-                           validation_steps=10, epochs=100, callbacks=[checkpoint, early])
+                        validation_steps=10, epochs=100, callbacks=[checkpoint, early])
 
 plt.plot(hist.history["acc"])
 plt.plot(hist.history['val_acc'])
