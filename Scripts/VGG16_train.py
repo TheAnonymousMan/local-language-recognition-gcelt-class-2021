128db492c0b135766ebb04aa7708193eb1cc887b
Souporno Ghosh <souporno2000@gmail.com>
2021-03-28 02:47:27 +0530

    Completed the presentation and the report except for summary of present work

--- Scripts/VGG16_train.py
+++ Scripts/VGG16_train.py
@@ -4,30 +4,36 @@ from keras.preprocessing import image
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
 
-#linear algebra mathematics
+# linear algebra mathematics
 import numpy as np
 
-#data representation, data store, data handling 
+# data representation, data store, data handling
 import pandas as pd
 
-#graphs drawing
+# graphs drawing
 import matplotlib.pyplot as plt
 
-#beautifies graphs based on seaborn
+# beautifies graphs based on seaborn
 import seaborn as sns
 
 # initialises seaborn
 sns.set()
 
+DataExtractionModel_VGG16 = VGG16()
+DataExtractionModel_VGG16.layers.pop()
 
 # top is not included as we are
 model = VGG16(weights='imagenet', include_top=False)
+# model = Model(inputs = DataExtractionModel_VGG16.inputs, outputs = DataExtractionModel_VGG16.layers[-1].output)
 
 # dealing with only feature extraction
 model.summary()
 
 # prepares images for loading into the model into the program
+
+
 def image_loader(img_path):
 
     # uses the keras preprocessing library to prepare the images for loading
@@ -48,21 +54,30 @@ def image_loader(img_path):
     print(img_data.shape)
 
     # converts the image array into a vector from a single dimensional array
-    img_data = np.expand_dims(img_data, axis=0) # google axis = 0
+    img_data = np.expand_dims(img_data, axis=0)  # google axis = 0
 
     # prints the shape
     print(img_data.shape)
 
-    # google 
+    # google
     img_data = preprocess_input(img_data)
 
     print(img_data.shape)
     return img_data
 
+
 img_path = '../Datasets/writerPair1/0-10/train/0/0000_01_0.tif'
 img_data = image_loader(img_path)
 
-#using the pretrained vgg16 model to output the feature matrix of one single image for testing
+# using the pretrained vgg16 model to output the feature matrix of one single image for testing
 model_feature = model.predict(img_data)
 
+feature_matrix = model_feature.flatten()
+
 print(model_feature.shape)
+print(feature_matrix.shape)
+
+np.save("feature.npy", model_feature)
+
+df = pd.DataFrame(feature_matrix)
+df.to_csv('feature.csv', index=False)
