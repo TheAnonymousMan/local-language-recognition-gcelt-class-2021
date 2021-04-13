d199244c669bc651cbbf8306d66e3295e699e944
YOUR_USERNAMErroy11705 <rroy11705@gmail.com>
2021-04-14 03:30:12 +0530

    train files divided in sets and Feature CSV is extracted

---
+++ Scripts/createSets.py
@@ -0,0 +1,17 @@
+import os, shutil
+
+directory = r'../Datasets'
+for f in os.listdir(directory):
+    for sf in os.listdir(f"{directory}/{str(f)}"):
+        for ssf in os.listdir(f"{directory}/{str(f)}/{sf}/train"):
+            for imageName in os.listdir(f"{directory}/{str(f)}/{sf}/train/{ssf}"):
+                folderName = imageName[:7]
+                folder_dir = f"{directory}/{str(f)}/{sf}/train/{ssf}/{folderName}"
+                if not os.path.exists(folder_dir):
+                    os.mkdir(folder_dir)
+                    os.chmod(folder_dir, 0o444)
+                    shutil.copy(os.path.join(f"{directory}/{str(f)}/{sf}/train/{ssf}/{imageName}"), f"{folder_dir}/{imageName}")
+                else:
+                    shutil.copy(os.path.join(f"{directory}/{str(f)}/{sf}/train/{ssf}/{imageName}"), f"{folder_dir}/{imageName}")
+            
+            print(f"Set {ssf} is completed.")
--- Scripts/testExtractions.py
+++ Scripts/testExtractions.py
@@ -1,24 +1,31 @@
 import FeatureDatasetExtractor as FDE
-import os
+import os, shutil
 
 
-def PrepareFeatureCsv(parentFolder, outputFolder):
-    for folderName in os.listdir(parentFolder):
-        inputFolder = parentFolder + '/' + folderName
-        fileName = outputFolder.split('.')[0]
-        print("extracting data from " + fileName)
-        outputFileName = outputFolder + '/' + fileName + '.csv'
+def PrepareFeatureCsv(inputFolder, outputFolder, outFile, inFile):
+        print(f"extracting data from {inFile}")
+        outputFileName = f"{outputFolder}/{outFile}.csv"
+        print(inputFolder, outputFileName)
         FDE.saveFeatureDatasetCSV(
             FDE.DataExtractionModel_VGG16, inputFolder,  outputFileName, FDE.TARGET_SIZE)
 
 
-base_directory = r'../Datasets'
-for folder in os.listdir(base_directory):
-    for subfolder in os.listdir(f"../Datasets/{str(folder)}"):
-        outFolder = "../CSVDatasets"
-        inFolder = f"../Datasets/{str(folder)}/{str(subfolder)}/train"
 
-        PrepareFeatureCsv(inFolder, outFolder)
-        print(f"{str(folder)} --> {str(subfolder)} Completed!")
+directory = r'../Datasets'
+languageCode = '001'
+for f in os.listdir(directory):
+    for sf in os.listdir(f"{directory}/{str(f)}"):
+        for ssf in os.listdir(f"{directory}/{str(f)}/{sf}/train"):
+            for item in os.listdir(f"{directory}/{str(f)}/{sf}/train/{ssf}"):
+                if '.tif' not in item:
+                    outFolder = f"{directory}/{str(f)}/{sf}/train"
+                    outFile=f"{item[:4]}_{languageCode}_{item[5:]}"
+                    inFolder = f"{directory}/{str(f)}/{sf}/train/{ssf}/{item}"
+
+                    PrepareFeatureCsv(inFolder, outFolder, outFile, item)
+                    print(f"{str(f)} --> {str(sf)} Completed!")
+
+            print(f"Set {ssf} is completed.")
+
                 
 
